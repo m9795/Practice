3c66487bef44ddf3f0cb20800ed877fe7ee38515
m9795 <m.ogawa9795@gmail.com>
2022-11-11 00:54:23 +0000

    [Add]配列とポインタ

--- hello.c
+++ hello.c
@@ -416,3 +416,48 @@ gcc -o xxx xxx.c -lm
 //     printf("ar2[%d]=%c * (p2+%d)=%c\n ",i,ar2[i],i,*(p2+i));
 //   }
 // }
+
+
+// #include <stdio.h>
+
+// void main(){
+//   // サイズSIZEの配列を用意する
+//   double d[3] = { 0.2 , 0.4 , 0.6 };
+//   double *p1 = NULL,*p2 = NULL;
+//   int i;
+//   p1 = d;
+//   p2 = d;
+//   for( i = 0; i < 3; i++){
+//     printf("%f %f %f\n",*(d+i),p1[i],*p2);
+//     p2++;  // p2のアドレスをインクリメント
+//   }
+// }
+
+
+// 動的なメモリの生成
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// #define SIZE  3
+
+// void main(){
+//   int* p1 = NULL;
+//   double *p2 = NULL;
+//   int i;
+//   // 配列の生成
+//   p1 = (int*)malloc(sizeof(int)*SIZE);
+//   p2 = (double*)malloc(sizeof(double)*SIZE);
+//   // 値の代入
+//   for(i = 0; i < SIZE; i++){
+//     p1[i] = i;
+//     p2[i] = i / 10.0;
+//   }
+//   // 結果の出力
+//   for(i = 0; i < SIZE; i++){
+//     printf("p1[%d]=%d p2[%d]=%f\n",i,p1[i],i,p2[i]);
+//   }
+//   // メモリの開放
+//   free(p1);
+//   free(p2);
+// }
