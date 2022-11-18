a568bbbdc7e43a0c27aff9b4952e2bd92ea27f79
m9795 <m.ogawa9795@gmail.com>
2022-11-18 15:58:39 +0000

    [Add]C++練習

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+class MyObject {
+public:
+  static int Calc(int x, int y);
+};
+
+int MyObject::Calc(int x, int y)
+{
+  return abs(x * y);
+}
+
+int main(int argc, char *argv[])
+{
+  printf("%d\n", MyObject::Calc(-1, 1));
+  printf("%d\n", MyObject::Calc(-2, -2));
+
+  return 0;
+}
